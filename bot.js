9f093d57cf5ccceabac094c723b8df9047d29e92
Camilo Pinilla <cristian.camilo6@gmail.com>
2020-04-06 08:12:55 -0500

    Saludos iniciales

--- bot.js
+++ bot.js
@@ -11,14 +11,18 @@ client.on('ready', () => {
 client.on('message', msg => {
   if (msg.content === '$reportediario') {
 
-  	var date = new Date().toLocaleString("es-CO")  	
+	var days = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sábado']
 
-  	msg.reply('ESTAMOS A ' + date)
+	var months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']  	
   	
-	msg.reply('----------')
+	msg.reply('---------- \n----------')
+
+	setTimeout(function(){
+		msg.reply('-')
+  	}, 3000);
 
   	setTimeout(function(){
-		msg.reply('----------')
+		msg.reply('---------- INICIO BOT ----------\n---------- INICIO BOT ----------')
   	}, 6000);
 
   	setTimeout(function(){
@@ -26,21 +30,36 @@ client.on('message', msg => {
   	}, 9000);
 
   	setTimeout(function(){
-		msg.reply('---------- INICIO BOT ----------')
+		msg.reply('Estamos a ' + days[new Date().getDay()] + ' ' + new Date().getDate() + ' de ' + months[new Date().getMonth()] + ' del ' + new Date().getFullYear())
   	}, 12000);
-
-  	setTimeout(function(){
-		msg.reply('---------- INICIO BOT ----------')
-  	}, 15000);
 
   	setTimeout(function(){
 		msg.reply('-')
   	}, 15000);
 
   	setTimeout(function(){
-		msg.reply('ESTAMOS A ' + new Date())
-  	}, 15000);
-   
+		msg.reply('Hola. Gracias por contactarme. ¡Estoy a tus ordenes! Voy a ayudarte a hacer rápidamente el informe diario para tu líder de equipo y las gerencias.')
+  	}, 18000);
+
+  	setTimeout(function(){
+		msg.reply('-')
+  	}, 21000);
+
+  	setTimeout(function(){
+		msg.reply('---------- AYER ----------\n---------- AYER ----------')
+  	}, 24000);
+
+  	setTimeout(function(){
+		msg.reply('-')
+  	}, 27000);
+
+  	setTimeout(function(){
+		msg.reply('Empecemos por AYER')
+  	}, 30000);
+
+  	setTimeout(function(){
+		msg.reply('Quedaron listas o completadas las siguientes tareas, que estaban programas para hacer AYER? responde SI o NO:')
+  	}, 33000);
   }
 });
 
